b86869ad1862eb518429776eb901fdda86aec869
Petrus Savolainen <petrus.savolainen@gmail.com>
2022-03-07 15:36:41 +0200

    Finally got VM with cloud init working, added PostgreSQL server too

--- azure-user-data.sh
+++ azure-user-data.sh
@@ -1,6 +1,2 @@
 #! /bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "<h1>Demo Bootstrapping Azure Virtual Machine</h1>" | sudo tee /var/www/html/index.html
+sudo apt update && sudo apt -y install apache2
