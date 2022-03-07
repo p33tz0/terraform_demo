6a90af26ca19d75a74a5fd2407f343fd3ecbc6e2
Petrus Savolainen <petrus.savolainen@gmail.com>
2022-03-07 10:57:56 +0200

    initial commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "petrusuusTFResourceGroup"
+}
+
+
+variable "subscription_id" { }
+variable "tenant_id" { }
+
+variable "resource_group_location" {
+  default = "westeurope"
+}
