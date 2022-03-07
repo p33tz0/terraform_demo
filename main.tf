0c79676e9302b21d1a1705b699502e39d0472cf5
Petrus Savolainen <petrus.savolainen@gmail.com>
2022-03-07 11:34:41 +0200

    Add subnet creation, storage account policy creation

--- main.tf
+++ main.tf
@@ -30,7 +30,29 @@ resource "azurerm_virtual_network" "vnet" {
     address_space       = ["10.0.0.0/16"]
     location            = "westeurope"
     resource_group_name = azurerm_resource_group.rg.name
+
+      subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
 }
+
+resource "azurerm_subnet" "snet" {
+  name                 = "petrussubnnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+}
+
 resource "azurerm_storage_account" "rg" {
   name                     = "petrus1234testi"
   resource_group_name      = var.resource_group_name
@@ -41,4 +63,28 @@ resource "azurerm_storage_account" "rg" {
   tags = {
     environment = "staging"
   }
+}
+
+
+resource "azurerm_storage_container" "rg" {
+  name                  = "content"
+  storage_account_name  = "petrus1234testi"
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_management_policy" "rg" {
+  storage_account_id ="${azurerm_storage_account.rg.id}"
+
+  rule {
+    name = "rule1"
+    enabled = true
+    filters {
+      blob_types = ["blockBlob"]
+    }
+    actions {
+      base_blob {
+        delete_after_days_since_modification_greater_than = 1
+      }
+  }
+}
 }
